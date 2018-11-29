5c6eec79f7ca8544f3c84fe6a821ce3c6189e6b3
Martin Hatas <martin.hatas@williamhill.com>
2018-11-29 19:41:39 +0100

    Fix some compile errors

--- src/main/scala/com/williamhill/fenix/server/channel/input/InputChannel.scala
+++ src/main/scala/com/williamhill/fenix/server/channel/input/InputChannel.scala
@@ -14,7 +14,7 @@ import com.williamhill.fenix.server.channel.messages.{ ChannelSubscribe, Channel
 import com.williamhill.fenix.server.messages._
 import rx.lang.scala.Observable
 
-trait InputChannel extends Config => Observable[WHOGatewayMessage]
+trait InputChannel extends (Config => Observable[WHOGatewayMessage])
 
 class KafkaInputChannel extends InputChannel with LazyLogging {
 
